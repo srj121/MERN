1bcf03d39d4f88517748cccaa6b9bb46b4bbcd70
Suraj Bade <115887200+srj121@users.noreply.github.com>
2023-04-20 13:00:47 +0530

    Add files via upload

---
+++ es6ArrowFunction.js
@@ -0,0 +1,11 @@
+// function multiply(a, b) {
+//     return a * b;
+// }
+// console.log(multiply(22, 10));
+
+// above code was done in js 
+
+const multiply = (a, b) => a * b;
+console.log(multiply(23, 56));
+
+//above code is done using es6
