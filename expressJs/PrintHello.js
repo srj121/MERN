1d371bab890c6fe798f3933ac708549cc3b70c92
Suraj Bade <115887200+srj121@users.noreply.github.com>
2023-04-20 22:37:37 +0530

    Add files via upload

---
+++ expressJs/PrintHello.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/', (req, res) => {
+    const name = req.query.name || 'World'
+    res.send(`hello ${name}!`)
+})
+
+app.listen(3000, () => {
+    console.log('server started on poort 3000')
+})
